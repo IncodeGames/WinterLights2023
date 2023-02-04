3d21774f4d49021f90630aa53345c013d146249a
Benjamin Lane <bplane2@incodegames.com>
2023-02-03 22:09:17 -0800

    Cap number of users and cull them when playing.

--- WinterLights2023/Assets/_Modules/Networking/ClientInterface.cs
+++ WinterLights2023/Assets/_Modules/Networking/ClientInterface.cs
@@ -5,17 +5,23 @@ using J38.WebSockets.Client;
 
 public class ClientInterface : MonoBehaviour
 {
+    private class User
+    {
+        public GameObject ufo;
+        public float lastUpdateTime;
+    }
+
     [SerializeField] private Transform turbTransform;
     [SerializeField] private GameObject[] ufoPrefabs;
     
-    private Dictionary<int, GameObject> clientUFOLookup = new Dictionary<int, GameObject>();
+    private Dictionary<int, User> clientUFOLookup = new Dictionary<int, User>();
 
     private const string url = "wss://ceti-wl-2023.glitch.me";
     private WebSocketClient webSocketClient;
 
     private Vector3 packetPosition;
 
-    private const int MAX_CONNECTIONS = 16;
+    private const int MAX_CONNECTIONS = 20;
 
     struct Packet
     {
@@ -39,9 +45,14 @@ public class ClientInterface : MonoBehaviour
     private void ProcessMessageReceived(string message)
     {
         Packet packet = JsonUtility.FromJson<Packet>(message);
-        if (!clientUFOLookup.ContainsKey(packet.id))
+        if (clientUFOLookup.Count + 1 <= MAX_CONNECTIONS)
         {
-            clientUFOLookup[packet.id] = Instantiate(ufoPrefabs[Random.Range(0, ufoPrefabs.Length)], Vector3.zero, Quaternion.identity);
+            if (!clientUFOLookup.ContainsKey(packet.id))
+            {
+                clientUFOLookup[packet.id] = new User();
+                clientUFOLookup[packet.id].ufo = Instantiate(ufoPrefabs[Random.Range(0, ufoPrefabs.Length)], Vector3.zero, Quaternion.identity);
+                clientUFOLookup[packet.id].lastUpdateTime = Time.time;
+            }
         }
 
         Debug.LogFormat("x: {0}, y: {1}", packet.x, packet.y);
@@ -55,7 +66,21 @@ public class ClientInterface : MonoBehaviour
         
         if (clientUFOLookup.ContainsKey(packet.id))
         {
-            clientUFOLookup[packet.id].transform.position = turbWorldPosition;
+            clientUFOLookup[packet.id].ufo.transform.position = turbWorldPosition;
+            clientUFOLookup[packet.id].lastUpdateTime = Time.time;
+        }
+    }
+
+    void Update()
+    {
+        foreach(KeyValuePair<int, User> entry in clientUFOLookup)
+        {
+            if (Time.time - entry.Value.lastUpdateTime > 15.0f)
+            {
+                Destroy(entry.Value.ufo);
+                clientUFOLookup.Remove(entry.Key);
+                return;
+            }
         }
     }
 
